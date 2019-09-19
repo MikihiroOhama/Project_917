bd7632ef136d03c03669e2ba905047c31270a473
MiroMiro <c19003@std.it-college.ac.jp>
2019-09-19 19:59:28 +0900

    init

---
+++ Assets/scripts/pause2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause2 : MonoBehaviour
+{
+    public GameObject reStartButton;
+    public GameObject pausePanel;
+    public GameObject ExitButton;
+    
+    public void StopGame()
+    {
+
+        Time.timeScale = 0f;
+        reStartButton.SetActive(true);
+        pausePanel.SetActive(true);
+        ExitButton.SetActive(true);
+    }
+    public void reStartGame()
+    {
+        reStartButton.SetActive(false);
+        pausePanel.SetActive(false);
+        ExitButton.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    
+}
